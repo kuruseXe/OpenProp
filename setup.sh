778f14df075dced9dd13cd2faa4c602a80a62c59
kuruse <uuusgr@gmail.com>
2020-09-02 16:58:43 +0900

    [update] add html template.

--- setup.sh
+++ setup.sh
@@ -31,14 +31,14 @@ if ! [ -e ${VIM_HOME:?} ]; then
     mkdir ${VIM_HOME:?}
 fi
 
-cp -r ${TOML_ORG:?} ${TOML_SET:?}
+cp -r ${TOML_ORG:?} ${VIM_HOME:?}/
 for toml_fl in $(ls -1 ${TOML_SET:?})
 do
     sed -i -e "s|__HOMEPATH__|${HOME}|g" ${TOML_SET:?}/${toml_fl:?}
 done
 echo "toml file set."
 
-cp -r ${TEMP_ORG:?} ${TEMP_SET:?}
+cp -r ${TEMP_ORG:?} ${VIM_HOME:?}/
 echo "template file set."
 
 echo "setup end."
